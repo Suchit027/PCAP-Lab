2eeef0451e7ea3169bf06d96d9a23e7c0b2b4cc4
suchit <110873739+Suchit027@users.noreply.github.com>
2025-01-11 18:06:20 +0000

    question 2 and 3

---
+++ lab 2/q3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+    int rank, size, ans;
+    int *arr = (int *)malloc(1000 * sizeof(int));
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int bsize = 1000;
+    int *buffer = (int *)malloc(bsize * sizeof(int));
+    MPI_Buffer_attach(buffer, bsize * sizeof(int));
+    if (rank == 0){
+        printf("enter the numbers\n");
+        for (int i = 0; i < size; ++i){
+            scanf("%d", &arr[i]);
+            MPI_Bsend(&arr[i], 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+        }
+    }
+    else{
+        MPI_Recv(&ans, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+        printf("%d for rank %d\n", ans * ans, rank);
+    }
+    MPI_Buffer_detach(&buffer, &bsize);
+    MPI_Finalize();
+    return 0;
+}
