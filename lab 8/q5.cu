ae870be4d5a3ec0fb0997e9e9306d45e0ecde41c
suchit <110873739+Suchit027@users.noreply.github.com>
2025-03-23 01:18:17 +0530

    additional left

--- lab 8/q5.cu
+++ lab 8/q5.cu
@@ -3,12 +3,12 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
-__global__ void mul(int *a, int *b, int *ans, int n1, int n2){
+__global__ void mul(int *a, int *b, int *ans, int m1, int n1, int n2){
     int id = (blockIdx.x * blockDim.x) + threadIdx.x;
-    for(int i = 0; i < n2; ++i){
-        ans[(id * n2) + i] = 0;
+    for(int i = 0; i < m1; ++i){
+        ans[(i * n2) + id] = 0;
         for(int j = 0; j < n1; ++j){
-            ans[(id * n2) + i] += a[(id * n1) + j] * b[(j * n2) + i];
+            ans[(i * n2) + id] += a[(i * n1) + j] * b[(j * n2) + id];
         }
     }
     return;
@@ -38,7 +38,7 @@ int main(){
     cudaMalloc((void **)&dans, sizeof(int) * m1 * n2);
     cudaMemcpy(da, a, sizeof(int) * m1 * n1, cudaMemcpyHostToDevice);
     cudaMemcpy(db, b, sizeof(int) * m2 * n2, cudaMemcpyHostToDevice);
-    mul<<<1, m1>>>(da, db, dans, n1, n2);
+    mul<<<1, n2>>>(da, db, dans, m1, n1, n2);
     cudaMemcpy(ans, dans, sizeof(int) * m1 * n2, cudaMemcpyDeviceToHost);
     for(int i = 0; i < m1; ++i){
         for(int j = 0; j < n2; ++j){
