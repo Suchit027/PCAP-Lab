5c9c540865015f169e174b1746964b5b85c410be
suchit <110873739+Suchit027@users.noreply.github.com>
2025-04-19 18:22:40 +0530

    some more corrections and better understanding of concept

--- lab 10/additional_q2.cu
+++ lab 10/additional_q2.cu
@@ -4,45 +4,44 @@
 #include <device_launch_parameters.h>
 
 #define MASK_RADIUS 2
-#define TILE_SIZE 8
+#define TILE_SIZE 5
 
-__constant__ int mask[10];
+__constant__ int mask[5];
 
-__global__ void conv1d(int *a, int input_l, int *ans)
+__global__ void conv1d(int *a, int *ans, int input_l)
 {
-    __shared__ int m[TILE_SIZE + (2 * MASK_RADIUS) + 1];
+    __shared__ int m[TILE_SIZE + (2 * MASK_RADIUS)];
     int i = (blockDim.x * blockIdx.x) + threadIdx.x;
     if (i < input_l)
     {
-        m[MASK_RADIUS + i] = a[i];
+        m[MASK_RADIUS + threadIdx.x] = a[i];
         if (threadIdx.x < MASK_RADIUS)
         {
             if (i - MASK_RADIUS >= 0)
             {
-                m[i] = a[i - MASK_RADIUS];
+                m[threadIdx.x] = a[i - MASK_RADIUS];
             }
-
             else
             {
-                m[i] = 0;
+                m[threadIdx.x] = 0;
             }
         }
-        if (threadIdx.x + MASK_RADIUS >= blockDim.x)
+        if (threadIdx.x + MASK_RADIUS >= TILE_SIZE)
         {
-            if (i + (2 * MASK_RADIUS) < input_l)
+            if (i + MASK_RADIUS < input_l)
             {
-                m[i + (2 * MASK_RADIUS)] = a[i + MASK_RADIUS];
+                m[threadIdx.x + (2 * MASK_RADIUS)] = a[i + MASK_RADIUS];
             }
-
             else
             {
-                m[i + (2 * MASK_RADIUS)] = 0;
+                m[threadIdx.x + (2 * MASK_RADIUS)] = 0;
             }
         }
         __syncthreads();
+        ans[i] = 0;
         for (int j = -MASK_RADIUS; j < MASK_RADIUS + 1; ++j)
         {
-            ans[i] += m[i + MASK_RADIUS + j] * mask[MASK_RADIUS + j];
+            ans[i] += m[threadIdx.x + MASK_RADIUS + j] * mask[MASK_RADIUS + j];
         }
     }
     return;
@@ -71,7 +70,7 @@ int main()
     cudaMemcpyToSymbol(mask, m, sizeof(int) * ((2 * MASK_RADIUS) + 1));
     cudaMemcpy(da, a, sizeof(int) * input_l, cudaMemcpyHostToDevice);
     dim3 gridSize((input_l + TILE_SIZE - 1) / TILE_SIZE);
-    conv1d<<<gridSize, TILE_SIZE>>>(da, input_l, dans);
+    conv1d<<<gridSize, TILE_SIZE>>>(da, dans, input_l);
     cudaMemcpy(ans, dans, sizeof(int) * input_l, cudaMemcpyDeviceToHost);
     printf("answer - \n");
     for (int i = 0; i < input_l; ++i)
--- lab 10/test.cu
+++ lab 10/test.cu
@@ -3,54 +3,46 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
+#define TILE_SIZE 5
+#define MASK_RADIUS 2
+
 __constant__ int mask[5];
 
-__global__ void conv1d(int *a, int *ans, int mask_l, int input_l)
+__global__ void conv1d(int *a, int *ans, int input_l)
 {
+    __shared__ int m[TILE_SIZE + (2 * MASK_RADIUS)];
     int i = (blockDim.x * blockIdx.x) + threadIdx.x;
-    int start = i - (mask_l / 2);
-    ans[i] = 0;
-    for (int j = 0; j < mask_l; ++j)
+    if (i < input_l)
     {
-        if (start + j >= 0 && start + j < input_l)
+        m[threadIdx.x + MASK_RADIUS] = a[i];
+        if (threadIdx.x < MASK_RADIUS)
         {
-            ans[i] += a[start + j] * mask[j];
+            if (i - MASK_RADIUS > 0)
+            {
+                m[threadIdx.x] = a[i - MASK_RADIUS];
+            }
+            else
+            {
+                m[threadIdx.x] = 0;
+            }
+        }
+        if (threadIdx.x + (2 * MASK_RADIUS) >= TILE_SIZE)
+        {
+            if (i + MASK_RADIUS < input_l)
+            {
+                m[threadIdx.x + (2 * MASK_RADIUS)] = a[i + MASK_RADIUS];
+            }
+            else
+            {
+                m[threadIdx.x + (2 * MASK_RADIUS)] = 0;
+            }
+        }
+        __syncthreads();
+        ans[i] = 0;
+        for (int j = -MASK_RADIUS; j < MASK_RADIUS + 1; ++j)
+        {
+            ans[i] += m[threadIdx.x + MASK_RADIUS + j] * mask[MASK_RADIUS + j];
         }
     }
     return;
-}
-int main()
-{
-    int *a, *ans, *da, *dans, *m, mask_l, input_l;
-    printf("enter input size\n");
-    scanf("%d", &input_l);
-    a = (int *)malloc(sizeof(int) * input_l);
-    ans = (int *)malloc(sizeof(int) * input_l);
-    printf("enter a\n");
-    for (int i = 0; i < input_l; ++i)
-    {
-        scanf("%d", &a[i]);
-    }
-    printf("enter mask size\n");
-    scanf("%d", &mask_l);
-    m = (int *)malloc(sizeof(int) * mask_l);
-    printf("enter mask\n");
-    for (int i = 0; i < mask_l; ++i)
-    {
-        scanf("%d", &m[i]);
-    }
-    cudaMalloc((void **)&da, sizeof(int) * input_l);
-    cudaMalloc((void **)&dans, sizeof(int) * input_l);
-    cudaMemcpyToSymbol(mask, m, sizeof(int) * mask_l);
-    cudaMemcpy(da, a, sizeof(int) * input_l, cudaMemcpyHostToDevice);
-    conv1d<<<1, input_l>>>(da, dans, mask_l, input_l);
-    cudaMemcpy(ans, dans, sizeof(int) * input_l, cudaMemcpyDeviceToHost);
-    printf("answer\n");
-    for (int i = 0; i < input_l; ++i)
-    {
-        printf("%d ", ans[i]);
-    }
-    cudaFree(da);
-    cudaFree(dans);
-    return 0;
 }
